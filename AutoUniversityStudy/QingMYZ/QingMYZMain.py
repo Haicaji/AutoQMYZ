f10a173a96fbed94fdb6e65ae851e937e230d2c9
31421 <1@1>
2024-04-15 11:44:15 +0800

    更改bat脚本存目录名

--- AutoUniversityStudy/QingMYZ/QingMYZMain.py
+++ AutoUniversityStudy/QingMYZ/QingMYZMain.py
@@ -220,8 +220,8 @@ class QingMYZClass():
         options.binary_location = f"{current_dir}\\ChromeWithDriver\\chrome.exe"
 
         # 设置无头浏览器
-        # options.add_argument('--headless')
-        # options.add_argument('--disable-gpu')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         
         # 忽略浏览器控制警告
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
